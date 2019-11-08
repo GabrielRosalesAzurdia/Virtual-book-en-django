df60f6dc3efce9a1516df1c0b642deea07f1aa23
Gabriel <gabrielalejandrorosalesazurdia@gmail.com>
2019-11-07 19:05:13 -0600

    con alert en static

--- virtualbook/settings.py
+++ virtualbook/settings.py
@@ -136,6 +136,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Rest Framework
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
